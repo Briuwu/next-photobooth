89086aac118a42c801684c27401a269067cd18fb
Briuwu <millontebrian@gmail.com>
2025-03-16 12:11:49 +0800

    Add conditional rendering for stickers based on image count in Axolotl, Cat, and Panda components

--- app/edit/cat-stickers.tsx
+++ app/edit/cat-stickers.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useImagesStore } from "@/providers/images-store-provider";
 import cat1 from "@/public/cat-stickers/angry.svg";
 import cat2 from "@/public/cat-stickers/eat.svg";
 import cat3 from "@/public/cat-stickers/full.svg";
@@ -12,18 +15,34 @@ import cat10 from "@/public/cat-stickers/yes.svg";
 import Image from "next/image";
 
 export const CatStickers = () => {
+  const { images } = useImagesStore((store) => store);
+
+  const isLessThanTwoImages = images.length < 2;
+  const isLessThanThreeImages = images.length < 3;
   return (
-    <div className="">
+    <div>
       <Image src={cat1} alt="" className="absolute top-4 w-12" />
       <Image src={cat2} alt="" className="absolute top-20 right-6 w-12" />
       <Image src={cat3} alt="" className="absolute top-40 left-6 w-12" />
-      <Image src={cat4} alt="" className="absolute top-60 right-6 w-12" />
-      <Image src={cat5} alt="" className="absolute top-80 left-6 w-12" />
-      <Image src={cat6} alt="" className="absolute top-96 left-32 w-12" />
-      <Image src={cat7} alt="" className="absolute right-6 bottom-56 w-12" />
-      <Image src={cat8} alt="" className="absolute bottom-36 left-6 w-12" />
+      {!isLessThanTwoImages && (
+        <Image src={cat4} alt="" className="absolute top-60 right-6 w-12" />
+      )}
+      {!isLessThanTwoImages && (
+        <Image src={cat5} alt="" className="absolute top-80 left-6 w-12" />
+      )}
+      {!isLessThanTwoImages && (
+        <Image src={cat6} alt="" className="absolute top-96 left-32 w-12" />
+      )}
+      {!isLessThanThreeImages && (
+        <Image src={cat7} alt="" className="absolute right-6 bottom-56 w-12" />
+      )}
+      {!isLessThanThreeImages && (
+        <Image src={cat8} alt="" className="absolute bottom-36 left-6 w-12" />
+      )}
       <Image src={cat9} alt="" className="absolute right-6 bottom-18 w-12" />
-      <Image src={cat10} alt="" className="absolute bottom-8 left-6 w-12" />
+      {!isLessThanTwoImages && (
+        <Image src={cat10} alt="" className="absolute bottom-8 left-6 w-12" />
+      )}
     </div>
   );
 };
--- app/edit/panda-stickers.tsx
+++ app/edit/panda-stickers.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useImagesStore } from "@/providers/images-store-provider";
 import panda1 from "@/public/panda-stickers/angry.svg";
 import panda2 from "@/public/panda-stickers/birthday.svg";
 import panda3 from "@/public/panda-stickers/eat.svg";
@@ -12,18 +15,38 @@ import panda10 from "@/public/panda-stickers/peace.svg";
 import Image from "next/image";
 
 export const PandaStickers = () => {
+  const { images } = useImagesStore((store) => store);
+
+  const isLessThanTwoImages = images.length < 2;
+  const isLessThanThreeImages = images.length < 3;
   return (
     <div>
       <Image src={panda1} alt="" className="absolute top-4 w-12" />
       <Image src={panda2} alt="" className="absolute top-20 right-6 w-12" />
       <Image src={panda3} alt="" className="absolute top-40 left-6 w-12" />
-      <Image src={panda4} alt="" className="absolute top-60 right-6 w-12" />
-      <Image src={panda5} alt="" className="absolute top-80 left-6 w-12" />
-      <Image src={panda6} alt="" className="absolute top-96 left-32 w-12" />
-      <Image src={panda7} alt="" className="absolute right-6 bottom-56 w-12" />
-      <Image src={panda8} alt="" className="absolute bottom-36 left-6 w-12" />
+      {!isLessThanTwoImages && (
+        <Image src={panda4} alt="" className="absolute top-60 right-6 w-12" />
+      )}
+      {!isLessThanTwoImages && (
+        <Image src={panda5} alt="" className="absolute top-80 left-6 w-12" />
+      )}
+      {!isLessThanTwoImages && (
+        <Image src={panda6} alt="" className="absolute top-96 left-32 w-12" />
+      )}
+      {!isLessThanThreeImages && (
+        <Image
+          src={panda7}
+          alt=""
+          className="absolute right-6 bottom-56 w-12"
+        />
+      )}
+      {!isLessThanThreeImages && (
+        <Image src={panda8} alt="" className="absolute bottom-36 left-6 w-12" />
+      )}
       <Image src={panda9} alt="" className="absolute right-6 bottom-18 w-12" />
-      <Image src={panda10} alt="" className="absolute bottom-8 left-6 w-12" />
+      {!isLessThanTwoImages && (
+        <Image src={panda10} alt="" className="absolute bottom-8 left-6 w-12" />
+      )}
     </div>
   );
 };
